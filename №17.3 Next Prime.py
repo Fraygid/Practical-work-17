4048d5b9d3dd6e40bdc3059562196ff286738eda
Fraygid <136465592+Fraygid@users.noreply.github.com>
2026-04-03 12:18:56 +0300

    Update №17.3 Next Prime.py

--- №17.3 Next Prime.py
+++ №17.3 Next Prime.py
@@ -1,1 +1,18 @@
-asd
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5)+1):
+        if num % i == 0:
+            return False
+    return True
+
+def get_next_prime(num):
+    while True:
+        num += 1
+        if is_prime(num):
+            return num
+
+
+print(get_next_prime(6))
+print(get_next_prime(7))
+print(get_next_prime(14))
