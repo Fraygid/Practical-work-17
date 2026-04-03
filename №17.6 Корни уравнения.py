98a86aa7c15ffd42ed4cf0a6dc7da7322f13a045
Fraygid <136465592+Fraygid@users.noreply.github.com>
2026-04-03 12:56:40 +0300

    Update №17.6 Корни уравнения.py

--- №17.6 Корни уравнения.py
+++ №17.6 Корни уравнения.py
@@ -1,1 +1,11 @@
-asd
+def solve(a,b,c):
+    d = b**2 - 4*a*c
+
+    x1 = (-b+d**0.5)/(2*a)
+    x2 = (-b-d**0.5)/(2*a)
+
+    return min(x1,x2), max(x1,x2)
+
+print(solve(1, -4, -5))
+print(solve(-2, 7, -5))
+print(solve(1, 2, 1))
